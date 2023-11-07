2cadfc8ae19c2090eaafdaaf3e611dea2bc67c98
Ali Ozen <ali.oezen@womensbest.com>
2023-11-07 11:25:28 +0100

    added env and postgres

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -12,6 +14,27 @@ type App struct {
 	DB     *sql.DB
 }
 
-func (a *App) Initialize(user, password, dbname string) {}
+func (a *App) Initialize() error {
+	connStr := getDBConnectionString()
+	fmt.Println(connStr)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return err
+	}
+
+	a.DB = db
+
+	return nil
+}
 
 func (a *App) Run(addr string) {}
+
+func getDBConnectionString() string {
+	return fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("APP_DB_HOST"), os.Getenv("APP_DB_PORT"), os.Getenv("APP_DB_NAME"), os.Getenv("APP_DB_USER"), os.Getenv("APP_DB_PASSWORD"))
+}
--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
+
+	err := loadEnvVariables()
+	if err != nil {
+		log.Fatal("Error loading environment variables: ", err)
+	}
+
 	myApp := App{}
-	myApp.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+	myApp.Initialize()
 
 	myApp.Run(":8080")
+}
+
+func loadEnvVariables() error {
+	file, err := os.Open(".env")
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		key, value := parts[0], parts[1]
+		os.Setenv(key, value)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
